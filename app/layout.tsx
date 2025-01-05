128f8b3a7d05ba1d9570e3525cb8ede566499da5
Saeed Mehmanparast <nextartisticdevelopments@gmail.com>
2025-01-05 23:35:50 +0330

    react query provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { Toaster } from 'sonner'
 import { cn } from '@/lib/utils'
+import QueryProviders from '@/providers/query-provider'
 const jakarta = Plus_Jakarta_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
@@ -21,15 +22,17 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body suppressHydrationWarning className={cn(jakarta.className, '')}>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-            <Toaster />
-          </ThemeProvider>
+          <QueryProviders>
+            <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+              {children}
+              <Toaster />
+            </ThemeProvider>
+          </QueryProviders>
         </body>
       </html>
     </ClerkProvider>
